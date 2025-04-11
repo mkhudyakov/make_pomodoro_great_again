e4cbc5679b05c5169788bdff9835883ce2a33f6f
DrKhu <khudyakov.max@gmail.com>
2025-04-11 17:58:50 -0400

    CHANGE: minor update

--- pomodoro.ino
+++ pomodoro.ino
@@ -6,6 +6,10 @@ BB_SPI_LCD lcd;
 enum State { WORK, BREAK };
 State currentState = WORK;
 
+const uint16_t bgColors[3] = {TFT_BLACK, TFT_BLUE, TFT_YELLOW};
+const uint16_t fontColorsNumbers[3] = {TFT_GREEN, TFT_WHITE, TFT_BLACK};
+const uint16_t fontColorsText[3] = {TFT_GREEN, TFT_WHITE, TFT_BLACK};
+
 const unsigned long WORK_DURATION = 25 * 60 * 1000UL;
 const unsigned long BREAK_DURATION = 10 * 60 * 1000UL;
 
@@ -16,9 +20,9 @@ bool running = true;
 void setup() {
   lcd.begin(DISPLAY_T_DONGLE_S3); // built-in config for LilyGO T-Dongle-S3
   lcd.setRotation(1); // Horizontal layout (160x80)
-  lcd.fillScreen(TFT_BLACK);
+  lcd.fillScreen(bgColors[0]);
   lcd.setFreeFont(&Roboto_Black_40);
-  lcd.setTextColor(TFT_GREEN);
+  lcd.setTextColor(fontColorsText[0]);
   // lcd.setFont(3);
 
   currentState = WORK;
@@ -36,7 +40,7 @@ void loop() {
     duration = (currentState == WORK) ? WORK_DURATION : BREAK_DURATION;
     startTime = millis();
 
-    lcd.fillScreen(TFT_BLACK);
+    lcd.fillScreen(bgColors[0]);
   }
 
   displayTimer(duration - (millis() - startTime));
@@ -47,15 +51,17 @@ void displayTimer(unsigned long timeLeft) {
   int minutes = (timeLeft / 1000) / 60;
   int seconds = (timeLeft / 1000) % 60;
 
+  int colorsMode = seconds % 2 > 0 ? 1: 2;
+
   char buf[16];
   sprintf(buf, "%02d:%02d", minutes, seconds);
 
-  lcd.fillScreen(TFT_BLACK);
+  lcd.fillScreen(minutes < 1 ? bgColors[colorsMode]: TFT_BLACK);
   lcd.setCursor(0, 40);
-  lcd.setTextColor((currentState == WORK) ? TFT_GREEN : TFT_CYAN);
+  lcd.setTextColor(minutes < 1 ? fontColorsNumbers[colorsMode]: ((currentState == WORK) ? TFT_GREEN : TFT_CYAN));
   lcd.print(buf);
 
   lcd.setCursor(0, 80);
-  lcd.setTextColor(TFT_WHITE);
+  lcd.setTextColor(minutes < 1 ? fontColorsText[colorsMode]: TFT_WHITE);
   lcd.print((currentState == WORK) ? "Work" : "Break");
 }
